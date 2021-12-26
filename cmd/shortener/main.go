3066c4deff04a81565864518a78d9c8ed9a8bd44
Egor <egr9898@gmail.com>
2021-12-26 17:49:33 +0300

    first increment
    (cherry picked from commit 04e39819bd83a5ca3f7c32b7169275310181d2de)

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,4 +1,16 @@
 package main
 
+import (
+	"github.com/Orel-AI/shortener.git/api/handler"
+	"github.com/Orel-AI/shortener.git/service/shortener"
+	"net/http"
+)
+
 func main() {
+	shortener.InitializeMap()
+	err := http.ListenAndServe(":8080", handler.ShortenerHandler{})
+	if err != nil {
+		panic(err)
+	}
+
 }
