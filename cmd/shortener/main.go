367522356bfdc8f0e54dc54c30f92e41ebbef6d7
Egor <egr9898@gmail.com>
2021-12-28 10:13:41 +0300

    Revert "first increment"
    This reverts commit 04e39819bd83a5ca3f7c32b7169275310181d2de.

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,16 +1,4 @@
 package main
 
-import (
-	"github.com/Orel-AI/shortener.git/api/handler"
-	"github.com/Orel-AI/shortener.git/service/shortener"
-	"net/http"
-)
-
 func main() {
-	shortener.InitializeMap()
-	err := http.ListenAndServe(":8080", handler.ShortenerHandler{})
-	if err != nil {
-		panic(err)
-	}
-
 }
