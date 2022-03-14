3002892807889291caac115399e3c9925598b626
Egor <egr9898@gmail.com>
2022-03-14 10:20:57 +0300

    added gzip handler

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"compress/gzip"
 	"github.com/Orel-AI/shortener.git/api/handler"
 	"github.com/Orel-AI/shortener.git/config"
 	"github.com/Orel-AI/shortener.git/service/shortener"
@@ -10,6 +11,19 @@ import (
 	"log"
 	"net/http"
 )
+
+var defaultCompressibleContentTypes = []string{
+	"text/html",
+	"text/css",
+	"text/plain",
+	"text/javascript",
+	"application/javascript",
+	"application/x-javascript",
+	"application/json",
+	"application/atom+xml",
+	"application/rss+xml",
+	"image/svg+xml",
+}
 
 func main() {
 
@@ -25,7 +39,7 @@ func main() {
 	r := chi.NewRouter()
 	r.Use(middleware.RequestID)
 	r.Use(middleware.Logger)
-	r.Use(middleware.Compress(5))
+	r.Use(middleware.Compress(gzip.DefaultCompression, defaultCompressibleContentTypes...))
 	r.Get("/{ID}", shortenerHandler.LookUpOriginalLinkGET)
 	r.Post("/", shortenerHandler.GenerateShorterLinkPOST)
 	r.Post("/api/shorten", shortenerHandler.GenerateShorterLinkPOSTJson)
